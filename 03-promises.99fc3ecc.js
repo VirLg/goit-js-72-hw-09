a9b47ac323763a7cb542cb7a85be310606efe0bc
VirLg <108211942+VirLg@users.noreply.github.com>
2023-02-23 17:26:35 +0000

    Deploying to gh-pages from @ VirLg/goit-js-72-hw-09@7a8fcd5bf4255b2f7a61c0c0b6bedd6b247be81a 🚀

---
+++ 03-promises.99fc3ecc.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("form");e.addEventListener("submit",(function(o){o.preventDefault();new FormData(e).forEach(((e,o)=>console.log(e,o)))}),{once:!0});new Promise;
+//# sourceMappingURL=03-promises.99fc3ecc.js.map
