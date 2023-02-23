dcfd837ee179e98280c7f33158cd5485b8410bd6
VirLg <108211942+VirLg@users.noreply.github.com>
2023-02-23 15:00:03 +0200

    add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,37 @@
 
+const refBody = document.querySelector('body')
+let refStartBTN = document.querySelector('button[data-start]');
+let refStopBTN = document.querySelector('button[data-stop]');
+const eventListenerBody = refBody.addEventListener('click', addEventListenerBTN, true)
+let idColorFunction = 0;
+refStopBTN.disabled = true;
+
+function addEventListenerBTN(evt){
+  if(evt.target.textContent === "Start"){
+    refStartBTN= eventListenerBody
+    evt.target.disabled = true;
+    evt.currentTarget.children[2].disabled = false
+    startChange()
+
+  }else if(evt.target.textContent === "Stop"){
+    refBody.removeEventListener('click',addEventListenerBTN)
+    evt.target.disabled = true;
+    evt.currentTarget.children[1].disabled = false
+    stopChange(idColorFunction)
+  }
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
+function startChange() {
+    idColorFunction = setInterval(()=>{
+   return refBody.style.backgroundColor = getRandomHexColor()
+},1000)
+return idColorFunction
+}
+
+function stopChange(idColorFunction) {
+    return clearInterval(idColorFunction)
+}
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,28 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+
+
+
+const refForm = document.querySelector('form');
+refForm.addEventListener('submit', onSubmitForm, { once: true })
+
+function onSubmitForm(evt) { 
+  evt.preventDefault();
+  const formData = new FormData(refForm)
+  formData.forEach((value,name)=>console.log(value,name))
+  
+}
+
+
+const promise = new Promise();
+
+
+// Promise.all()
+
+
+// const Data = new Data()
+
+
+// console.log(refForm);
